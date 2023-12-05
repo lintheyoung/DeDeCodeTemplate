c8e24d8dcd5827bb13d2ca966b6e50778712a2bd
HenryLin <102785622+lintheyoung@users.noreply.github.com>
2023-12-05 13:52:27 +0800

    FSMManager支持各个State内部的互相调用

--- FSMStateManager/FSMStateManager.ino
+++ FSMStateManager/FSMStateManager.ino
@@ -5,51 +5,67 @@ typedef void (*StateAction)();
 
 class FSMManager {
 private:
-  SimpleFSM fsm;
-  State initialState;
-  State states[5];
-  Transition* transitions;
-  int numTransitions;
+    SimpleFSM fsm;
+    State initialState;
+    State states[5];
+    Transition* transitions;
+    int numTransitions;
+    int nextTrigger; // 存储下一个触发器的变量
 
 public:
-  FSMManager(StateAction introActions[5], StateAction runActions[5], StateAction outroActions[5])
-    : initialState("InitialState", []() {}) {
-    // 初始化状态
-    for (int i = 0; i < 5; i++) {
-      states[i] = State("State" + String(i + 1), introActions[i], runActions[i], outroActions[i]);
+    // 无参构造函数
+    FSMManager() : initialState("InitialState", []() {}), numTransitions(0), transitions(nullptr), nextTrigger(0) {}
+
+    // 析构函数
+    ~FSMManager() {
+        delete[] transitions;
     }
 
-    // 初始化转换逻辑
-    numTransitions = 5 + 1 * 4 * 5;  // 5个初始转换 + 20个交叉转换
-    transitions = new Transition[numTransitions];
-    int t = 0;
-
-    // 填充转换逻辑
-    for (int i = 0; i < 5; i++) {
-      transitions[t++] = Transition(&initialState, &states[i], i + 1);
-      for (int j = 0; j < 5; j++) {
-        if (i != j) {
-          transitions[t++] = Transition(&states[i], &states[j], j + 1);
+    // 设置状态和转换的方法
+    void setup(StateAction introActions[5], StateAction runActions[5], StateAction outroActions[5]) {
+        // 初始化状态
+        for (int i = 0; i < 5; i++) {
+            states[i] = State("State" + String(i + 1), introActions[i], runActions[i], outroActions[i]);
         }
-      }
+
+        // 初始化转换逻辑
+        numTransitions = 5 + 1 * 4 * 5;  // 5个初始转换 + 20个交叉转换
+        delete[] transitions;  // 清除之前的转换（如果有）
+        transitions = new Transition[numTransitions];
+        int t = 0;
+
+        // 填充转换逻辑
+        for (int i = 0; i < 5; i++) {
+            transitions[t++] = Transition(&initialState, &states[i], i + 1);
+            for (int j = 0; j < 5; j++) {
+                if (i != j) {
+                    transitions[t++] = Transition(&states[i], &states[j], j + 1);
+                }
+            }
+        }
+
+        fsm.add(transitions, numTransitions);
+        fsm.setInitialState(&initialState);
     }
 
-    fsm.add(transitions, numTransitions);
-    fsm.setInitialState(&initialState);
-  }
+    // 触发器方法
+    void trigger(int t) {
+        nextTrigger = t; // 设置下一个触发器
+    }
 
-  ~FSMManager() {
-    delete[] transitions;
-  }
+    // 运行状态机
+    void run(int delayTime) {
+        fsm.run(delayTime);
 
-  void trigger(int t) {
-    fsm.trigger(t);
-  }
-
-  void run(int delayTime) {
-    fsm.run(delayTime);
-  }
+        // 检查是否有待处理的触发器
+        if (nextTrigger != 0) {
+            fsm.trigger(nextTrigger);
+            nextTrigger = 0; // 重置触发器
+        }
+    }
 };
+
+FSMManager fsmManager;
 
 // 示例：用户自定义的函数
 void customIntro1() {
@@ -97,28 +113,33 @@ void customIntro5() {
 }
 void customRun5() {
   Serial.println("Custom Run 5");
+  fsmManager.trigger(1);
 }
 void customOutro5() {
   Serial.println("Custom Outro 5");
+  
 }
 
-// 为每个状态创建自定义函数数组
-StateAction intros[5] = { customIntro1, customIntro2, customIntro3, customIntro4, customIntro5 };
-StateAction runs[5] = { customRun1, customRun2, customRun3, customRun4, customRun5 };
-StateAction outros[5] = { customOutro1, customOutro2, customOutro3, customOutro4, customOutro5 };
 
-// 创建FSMManager实例
-FSMManager fsmManager(intros, runs, outros);
 
 void setup() {
   Serial.begin(115200);
   delay(1000);
+
+  // 为每个状态创建自定义函数数组
+  StateAction intros[5] = { customIntro1, customIntro2, customIntro3, customIntro4, customIntro5 };
+  StateAction runs[5] = { customRun1, customRun2, customRun3, customRun4, customRun5 };
+  StateAction outros[5] = { customOutro1, customOutro2, customOutro3, customOutro4, customOutro5 };
+
+  // 创建FSMManager实例
+  fsmManager.setup(intros, runs, outros);
+
   Serial.println("FSM Ready");
 }
 
 void loop() {
   // 运行状态机逻辑
-  fsmManager.run(500);
+  fsmManager.run(5);
 
   // 处理串口命令
   if (Serial.available() > 0) {
